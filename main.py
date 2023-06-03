a6268b0f2287fc1c95f33cc60b46ea881f7f2724
André Júlio Moreira <andre.moreira.9@hotmail.com>
2023-06-03 10:46:26 +0100

    Improve color assignment

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import time
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +9,10 @@ NAME = "LaunchPa$"
 CAM_IDX = 2
 
 # def detect_circles():
+
+COLOR_DIST_TRESHOLD = 30
+COLOR_DETECTION_TIMEOUT = 2
+NUM_COLORS = 8
 
 cam = cv2.VideoCapture(CAM_IDX)
 cv2.namedWindow(NAME)
@@ -32,6 +38,34 @@ def find_circle_color(img, x, y, r):
         data.append(int(color))
     return tuple(data)
 
+class Button:
+    def __init__(self, color: tuple, position: tuple, id: int) -> None:
+        self.color: tuple = color
+        self.position: tuple = position
+        self.id: int = id
+
+buttons = {}
+
+
+def distance(c1, c2):
+    (r1,g1,b1) = c1
+    (r2,g2,b2) = c2
+    return math.sqrt((r1 - r2)**2 + (g1 - g2) ** 2 + (b1 - b2) **2)
+
+def find_closest_color(color):
+    if len(buttons) == 0:
+        return None
+
+    res = sorted(buttons, key=lambda target: distance(color, target))
+    target = res[0]
+    if distance(color, target) < COLOR_DIST_TRESHOLD or len(buttons.keys()) == NUM_COLORS:
+        return target
+
+    return None
+
+
+initial_time = time.time()
+
 while True:
     _, img = cam.read()
 
@@ -39,12 +73,23 @@ while True:
     # gray_img = cv2.medianBlur(gray_img, 5)
     circles = find_circles_positions(gray_img)
     
-    if circles is not None:
+    if circles is not None and time.time() > initial_time + COLOR_DETECTION_TIMEOUT:
         circles = np.uint16(np.around(circles))
         for [pos_x, pos_y, radius] in circles[0,:]:
+            bounding_box = [(pos_x - radius, pos_y - radius), (pos_x + radius, pos_y + radius)]
             color = find_circle_color(img, pos_x, pos_y, radius)
-            cv2.circle(img, (pos_x, pos_y), radius, color, thickness=cv2.FILLED)
-            cv2.circle(img, (pos_x, pos_y), radius, (0,0,0), thickness=2)
+
+            similar_color = find_closest_color(color)
+
+            if similar_color is None:
+                btn = Button(color, (pos_x, pos_y), len(buttons))
+                buttons[color] = btn
+            else:
+                btn = buttons[similar_color]
+                btn.position = (pos_x, pos_y)
+            cv2.rectangle(img, bounding_box[0], bounding_box[1], color, thickness=cv2.FILLED)
+            cv2.rectangle(img, bounding_box[0], bounding_box[1], (0,0,0), thickness=2)
+            cv2.putText(img, str(btn.id), org=(pos_x, pos_y), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(0,0,0), thickness=1)
 
     cv2.imshow(NAME, img)
 
